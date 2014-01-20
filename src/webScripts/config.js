39838b29c58c92f71a80e43e03cf0a65544967b9
John Garland <john@openlearning.com>
2014-01-20 14:00:21 +1100

    Basic flash embedding (based on URL)

---
+++ src/webScripts/config.js
@@ -0,0 +1,29 @@
+if (request.method === 'GET') {
+   var data = OpenLearning.page.getData(request.user);
+   if (data.success) {
+      var config = data.data || {};
+      config.success = true;
+      response.writeJSON(config);
+   } else {
+      response.setStatusCode(422);
+      response.writeJSON({'error': (data.reason || "Unknown error")});
+   }
+} else if (request.method === 'PUT') {
+   var hasWritePermission = request.sessionData.permissions && (request.sessionData.permissions.indexOf('write') >= 0);
+   if (hasWritePermission) {
+      var result = OpenLearning.page.setData(request.data, request.user);
+      if (result.success) {
+         response.writeJSON({'success': true});
+      } else {
+         response.setStatusCode(422);
+         response.writeJSON({'error': (result.reason || "Unknown error")});
+      }
+   } else {
+      response.setStatusCode(403);
+      response.writeJSON({'error': "Insufficient permissions to save config"});
+   }
+} else {
+   response.setStatusCode(405);
+   response.setHeader('Allow', 'GET, PUT');
+   response.writeJSON({'error': "Not allowed method: " + request.method});
+}
